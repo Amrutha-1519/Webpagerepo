fe07ec5bcc2b15f3673afa7285e8c1425fbe5d08
Amrutha <hegdeamrutha1519@gmail.com>
2025-10-03 22:13:17 +0530

    New wbsite

---
+++ pagescript.js
@@ -0,0 +1,11 @@
+function agreed() {
+  alert(
+    "👍 Thankyou for your feedback!!!We will continue our service"
+  );
+}
+
+function notagreed(){
+    alert(
+        "😒 Sorryyy!!!!!We will try to update the website"
+    );
+}
